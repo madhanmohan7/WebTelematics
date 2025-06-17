632fa5faf7525deb3d4f3adb0966ded5a3730343
Madhan Mohan <126551992+madhanmohan7@users.noreply.github.com>
2025-06-17 11:36:26 +0530

    Update api.dart
    replacing old ip address with new ip address

--- lib/src/services/api.dart
+++ lib/src/services/api.dart
@@ -1,26 +1,28 @@
 class BaseURLConfig {
 
-  static const String speedApiURL = "http://34.47.250.136:3000/telematics/latest-speed/";
+  static const String ipAddress = "34.47.242.135";
+  
+  static const String speedApiURL = "http://$ipAddress:3000/telematics/latest-speed/";
 
-  static const String rssiApiURL = "http://34.47.250.136:3000/telematics/signal-strength/";
+  static const String rssiApiURL = "http://$ipAddress:3000/telematics/signal-strength/";
 
-  static const String batterySOCApiURL = "http://34.47.250.136:3000/telematics/latest-soc/";
+  static const String batterySOCApiURL = "http://$ipAddress:3000/telematics/latest-soc/";
 
-  static const String accelApiURL = "http://34.47.250.136:3000/telematics/latest-accel/";
+  static const String accelApiURL = "http://$ipAddress:3000/telematics/latest-accel/";
 
-  static const String gyroApiURL = "http://34.47.250.136:3000/telematics/latest-gyro/";
+  static const String gyroApiURL = "http://$ipAddress:3000/telematics/latest-gyro/";
 
-  static const String locationApiURL = "http://34.47.250.136:3000/telematics/latest-location/";
+  static const String locationApiURL = "http://$ipAddress:3000/telematics/latest-location/";
 
   //RAW CAN DATA APIs
-  static const String vehicleInfoOneApiURL = "http://34.47.250.136:3000/telematics/vehicle-info1/";
-  static const String driveInfoApiURL = "http://34.47.250.136:3000/telematics/drive-info/";
-  static const String vehicleInfoTwoApiURL = "http://34.47.250.136:3000/telematics/vehicle-info2/";
-  static const String diagnosisApiURL = "http://34.47.250.136:3000/telematics/diagnosis/";
-  static const String odoApiURL = "http://34.47.250.136:3000/telematics/odo/";
-  static const String bmsApiURL = "http://34.47.250.136:3000/telematics/bms/";
-  static const String epsOneApiURL = "http://34.47.250.136:3000/telematics/eps1/";
-  static const String epsTwoApiURL = "http://34.47.250.136:3000/telematics/eps2/";
+  static const String vehicleInfoOneApiURL = "http://$ipAddress:3000/telematics/vehicle-info1/";
+  static const String driveInfoApiURL = "http://$ipAddress:3000/telematics/drive-info/";
+  static const String vehicleInfoTwoApiURL = "http://$ipAddress:3000/telematics/vehicle-info2/";
+  static const String diagnosisApiURL = "http://$ipAddress:3000/telematics/diagnosis/";
+  static const String odoApiURL = "http://$ipAddress:3000/telematics/odo/";
+  static const String bmsApiURL = "http://$ipAddress:3000/telematics/bms/";
+  static const String epsOneApiURL = "http://$ipAddress:3000/telematics/eps1/";
+  static const String epsTwoApiURL = "http://$ipAddress:3000/telematics/eps2/";
 
 
 
